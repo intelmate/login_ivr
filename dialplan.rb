b83b8b11b5cdfbbf8cf4360ede2c0768a2fac47d
root <root@ics-oregon.localdomain>
2008-09-16 17:05:18 -0600

    Login IVR now complete for first pass at testing

--- dialplan.rb
+++ dialplan.rb
@@ -1,3 +1,4 @@
+#The main context to obtain our details, authentication and ultimate login to queues
 from_internal {
   answer
   
@@ -29,10 +30,9 @@ from_internal {
   
   +fetch_agent_username
   
-  #execute("AgentCallbackLogin", agent_login, "", callerid.to_s + "@from-internal")
-  
 }
 
+#Context to obtain the agent login name
 fetch_agent_username {
   
   cnt = 0
@@ -41,28 +41,35 @@ fetch_agent_username {
   while cnt < 3 do
     @agent_username = input 5, :timeout => 5.seconds, :play => "agent-user", :accept_key => "#"
     cnt += 1
+    #Lets loop through each of the agent entries to see if we have a match
     @agents.each do |agent|
       if agent[:login] == @agent_username
         valid_agent = TRUE
         cnt = 10
         break
       else
+        #Lets keep track of which agent we are at so we do not have to search in the next method and
+        #may reference diretly
         @which_agent += 1
       end
     end
-    if valid_agent == FALSE
+    #If we still do not have the right agent entered and have not depleted our retries play an 
+    #incorrect message back to the caller
+    if valid_agent == FALSE && cnt != 3
       play "agent-incorrect"
     end
   end
-   
+  
+  #If the agent is valid then lets try for the password, otherwise hangup on the caller
   if valid_agent == TRUE
     +fetch_agent_passwd
   else
-    play 'goodbye'
+    play "vm-goodbye"
     hangup
   end
 }
 
+#Method to fetch the agent password
 fetch_agent_passwd {
 
   cnt = 0
@@ -70,19 +77,35 @@ fetch_agent_passwd {
   while cnt < 3 do
     agent_passwd = input 5, :timeout => 5.seconds, :play => "agent-pass", :accept_key => "#"
     cnt += 1
+    #If the pin entered matches the one to the agent entered then we have a valid login
     if @agents[@which_agent][:pin] == agent_passwd
       valid_agent = TRUE
       cnt = 10
     end
-    if valid_agent == FALSE
+    if valid_agent == FALSE && cnt != 3
       play "agent-incorrect"
     end
   end
    
   if valid_agent == TRUE
-    play 'tt-monkeys'
+    #If the caller entered by dialing *50 they are logging into the queue, if *51 they are leaving the queue
+    if extension == "*50"
+      @queues.each do |queue|
+        execute("AddQueueMember", "#{queue}|SIP/#{callerid}")
+      end
+      play "agent-loginok"
+      play "vm-goodbye"
+      hangup
+    elsif extension == "*51"
+      @queues.each do |queue|
+        execute("RemoveQueueMember", "#{queue}|SIP/#{callerid}")
+      end
+      play "agent-loggedoff"
+      play "vm-goodbye"
+      hangup
+    end
   else
-    play 'goodbye'
+    play "vm-goodbye"
     hangup
   end
 }
